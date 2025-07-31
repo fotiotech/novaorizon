3a1f1a8bb0ebc2a40933fe05ccde3ab671089018
dylan/ <dilanefotio14@gmail.com>
2025-07-31 18:34:46 +0100

    gig of today

--- src/app/(root)/checkout/page.tsx
+++ src/app/(root)/checkout/page.tsx
@@ -122,9 +122,9 @@ const CheckoutPage = () => {
         </div>
       </div>
 
-      <div>
+      {/* <div>
         <OrderButton paymentMethod={customerInfos?.billingMethod?.methodType} />
-      </div>
+      </div> */}
       <div className="mt-4">
         <Link href={`/checkout/chat`}>
           <button className="bg-blue-600 w-full text-white px-4 py-2 rounded">
