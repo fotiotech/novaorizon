8451ab6bb49509bd0f5f60dcb27b3b7871cdba61
dylan/ <dilanefotio14@gmail.com>
2025-07-31 22:50:42 +0100

    gig of today

--- src/app/actions/monetbil_payment.ts
+++ src/app/actions/monetbil_payment.ts
@@ -16,8 +16,7 @@ export async function generatePaymentLink(
       `https://api.monetbil.com/widget/v2.1/${paymentData.serviceKey}`,
       {
         amount: paymentData.amount,
-        // phone: paymentData.phone,
-        phone_lock: paymentData.phoneLock || false,
+        phone: paymentData.phone,
         locale: paymentData.locale || "en",
         operator: paymentData.operator || "CM_MTNMOBILEMONEY",
         country: "CM",
