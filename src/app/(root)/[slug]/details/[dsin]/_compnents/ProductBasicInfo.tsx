86920eaf82f716b66ea69156ebe6b0cbbc56196f
dylan/ <dilanefotio14@gmail.com>
2025-09-13 06:32:06 +0100

    new gig

--- src/app/(root)/[slug]/details/[dsin]/_compnents/ProductBasicInfo.tsx
+++ src/app/(root)/[slug]/details/[dsin]/_compnents/ProductBasicInfo.tsx
@@ -32,7 +32,7 @@ const ProductBasicInfo: React.FC<ProductBasicInfoProps> = ({
         {Array.isArray(gallery) && gallery.length > 0 ? (
           <div className="md:w-1/2">
             {brand?.name && (
-              <Link href={`/brandStore?brandId=${_id}`} className="mb-2">
+              <Link href={`/brandStore?brandId=${_id}`} className="">
                 visit <span className="text-blue-600">{brand?.name}</span>
               </Link>
             )}
